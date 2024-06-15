24df06099f1b1dc2e85a03d134cf881a61d2f144
davebulaval <david.beauchemin.5@ulaval.ca>
2024-06-15 18:22:38 -0400

    better results output

--- article_src/llm/bloom.py
+++ article_src/llm/bloom.py
@@ -30,24 +30,25 @@ for lang in [
     "no",
     "ja",
 ]:
-    print(f"Language: {lang}")
-    data_dir = os.path.join(datastore_dir, f"{lang}-cola")
-    dev_dataset = load_dataset(data_dir, data_files=["dev.tsv"])
-    test_dataset = load_dataset(data_dir, data_files=["test.tsv"])
+    with open(f"{lang}-boom.txt", "w") as file:
+        print(f"Language: {lang}", file=file)
+        data_dir = os.path.join(datastore_dir, f"{lang}-cola")
+        dev_dataset = load_dataset(data_dir, data_files=["dev.tsv"])
+        test_dataset = load_dataset(data_dir, data_files=["test.tsv"])
 
-    dev_dataset = dev_dataset.map(pipe_fn)
-    test_dataset = test_dataset.map(pipe_fn)
+        dev_dataset = dev_dataset.map(pipe_fn)
+        test_dataset = test_dataset.map(pipe_fn)
 
-    dev_accuracy = (
-        sum(dev_dataset["train"]["good_prediction"])
-        / len(dev_dataset["train"]["good_prediction"])
-        * 100
-    )
-    test_accuracy = (
-        sum(test_dataset["train"]["good_prediction"])
-        / len(test_dataset["train"]["good_prediction"])
-        * 100
-    )
+        dev_accuracy = (
+            sum(dev_dataset["train"]["good_prediction"])
+            / len(dev_dataset["train"]["good_prediction"])
+            * 100
+        )
+        test_accuracy = (
+            sum(test_dataset["train"]["good_prediction"])
+            / len(test_dataset["train"]["good_prediction"])
+            * 100
+        )
 
-    print(f"Dev acc: {dev_accuracy}")
-    print(f"Test acc: {test_accuracy}")
+        print(f"Dev acc: {dev_accuracy}", file=file)
+        print(f"Test acc: {test_accuracy}", file=file)
