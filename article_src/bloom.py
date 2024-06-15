7d34b1b479b56f7a3a2d2a45a5939bceb1ecf035
davebulaval <david.beauchemin.5@ulaval.ca>
2024-06-15 17:50:39 -0400

    add llm eval

--- article_src/bloom.py
+++
@@ -1,8 +0,0 @@
-from transformers import pipeline
-
-pipe = pipeline(task="zero-shot-classification", model="bigscience/bloom")
-
-pipe(
-    "I have a problem with my iphone that needs to be resolved asap!",
-    candidate_labels=["Grammatical", "Ungrammatical"],
-)
---
+++ article_src/llm/llama.py
@@ -0,0 +1,48 @@
+import os
+from functools import partial
+
+from datasets import load_dataset
+from transformers import pipeline
+
+from tools import predict
+
+pipe = pipeline(
+    task="zero-shot-classification", model="meta-llama/Llama-2-7b-hf", device=0
+)
+
+root = ".."
+datastore_dir = os.path.join(root, "datastore", "cola_datasets")
+
+pipe_fn = partial(predict, pipe=pipe)
+
+for lang in [
+    "fr",
+    "en",
+    "it",
+    "ru",
+    "sv",
+    "zh",
+    "no",
+    "ja",
+]:
+    print(f"Language: {lang}")
+    data_dir = os.path.join(datastore_dir, f"{lang}-cola")
+    dev_dataset = load_dataset(data_dir, data_files=["dev.tsv"])
+    test_dataset = load_dataset(data_dir, data_files=["test.tsv"])
+
+    dev_dataset = dev_dataset.map(pipe_fn)
+    test_dataset = test_dataset.map(pipe_fn)
+
+    dev_accuracy = (
+        sum(dev_dataset["train"]["good_prediction"])
+        / len(dev_dataset["train"]["good_prediction"])
+        * 100
+    )
+    test_accuracy = (
+        sum(test_dataset["train"]["good_prediction"])
+        / len(test_dataset["train"]["good_prediction"])
+        * 100
+    )
+
+    print(f"Dev acc: {dev_accuracy}")
+    print(f"Test acc: {test_accuracy}")
