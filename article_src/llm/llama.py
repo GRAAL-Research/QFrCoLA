8296604d9a1e0cb40ef9fd5225222f8e39149f23
davebulaval <david.beauchemin.5@ulaval.ca>
2024-06-15 23:21:45 -0400

    add fr category eval

--- article_src/llm/llama.py
+++ article_src/llm/llama.py
@@ -1,6 +1,7 @@
 import os
 from functools import partial
 
+import numpy as np
 from datasets import load_dataset
 from dotenv import dotenv_values
 from evaluate import load
@@ -48,18 +49,20 @@ for lang in [
 
         dev_dataset = dev_dataset.map(pipe_fn)
 
-        predictions = dev_dataset["train"]["prediction"]
-        labels = dev_dataset["train"]["label"]
-        dev_accuracy = ACCURACY.compute(predictions=predictions, references=labels)
-        dev_mcc = MCC.compute(predictions=predictions, references=labels)
+        dev_predictions = dev_dataset["train"]["prediction"]
+        dev_labels = dev_dataset["train"]["label"]
+        dev_accuracy = ACCURACY.compute(
+            predictions=dev_predictions, references=dev_labels
+        )
+        dev_mcc = MCC.compute(predictions=dev_predictions, references=dev_labels)
 
         print(f"Dev acc: {dev_accuracy}\n", file=file)
         print(f"Dev MCC: {dev_mcc}\n", file=file)
 
         test_dataset = test_dataset.map(pipe_fn)
 
-        predictions = test_dataset["train"]["prediction"]
-        labels = test_dataset["train"]["label"]
+        test_predictions = test_dataset["train"]["prediction"]
+        test_labels = test_dataset["train"]["label"]
         test_accuracy = ACCURACY.compute(predictions=predictions, references=labels)
         test_mcc = MCC.compute(predictions=predictions, references=labels)
 
@@ -67,6 +70,51 @@ for lang in [
         print(f"Test MCC: {test_mcc}\n", file=file)
 
         if lang == "fr":
+            dev_categories = dev_dataset["train"]["category"]
+            test_categories = test_dataset["train"]["category"]
+
+            for category_name in ["semantic", "syntax", "morphology", "anglicism"]:
+                dev_cat_idxs = [
+                    idx
+                    for idx, category in enumerate(dev_categories)
+                    if category == category_name
+                ]
+                dev_cat_labels = np.array(dev_labels)[dev_cat_idxs]
+                dev_cat_pred = np.array(dev_predictions)[dev_cat_idxs]
+
+                dev_accuracy_cat = ACCURACY.compute(
+                    predictions=dev_cat_pred, references=dev_cat_labels
+                )
+                dev_mcc_cat = MCC.compute(
+                    predictions=dev_cat_pred, references=dev_cat_labels
+                )
+
+                print(
+                    f"Dev category {category_name} acc: {dev_accuracy_cat}\n", file=file
+                )
+                print(f"Dev category {category_name} MCC: {dev_mcc_cat}\n", file=file)
+
+                test_cat_idxs = [
+                    idx
+                    for idx, category in enumerate(test_categories)
+                    if category == category_name
+                ]
+                test_cat_labels = np.array(test_labels)[test_cat_idxs]
+                test_cat_pred = np.array(test_predictions)[test_cat_idxs]
+
+                test_accuracy_cat = ACCURACY.compute(
+                    predictions=test_cat_pred, references=test_cat_labels
+                )
+                test_mcc_cat = MCC.compute(
+                    predictions=test_cat_pred, references=test_cat_labels
+                )
+
+                print(
+                    f"Test category {category_name} acc: {test_accuracy_cat}\n",
+                    file=file,
+                )
+                print(f"Test category {category_name} MCC: {test_mcc_cat}\n", file=file)
+
             ood_dataset = load_dataset(data_dir, data_files=["ood.tsv"])
             ood_dataset = ood_dataset.map(pipe_fn)
 
